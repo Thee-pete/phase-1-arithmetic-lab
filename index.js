82a43dee04750ec8abc67a5a85a74c63829a624b
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-07 00:00:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Math.round(Math.random() * 10);
+
+let num3 = 54;
+let num4 = 10;
+const mod = num3%num4;
+
+
+const max = Math.max(1,14, 6, 20);
+
+
